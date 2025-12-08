84d45b82694cf6a8a8c1c4f63b34ce0851729ed7
IT-Bill <itbill8888@gmail.com>
2025-12-09 00:36:43 +0800

    pref: optimize token counting in _pre_tokenize function

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -59,7 +59,7 @@ def _pre_tokenize(text: str, special_tokens: list[str]) -> dict[tuple[bytes, ...
         r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
     )
     BYTE_TABLE: tuple[bytes, ...] = tuple(bytes((i,)) for i in range(256))
-
+    
     sub_chunks = _remove_special_tokens(text, special_tokens)
 
     token_count: dict[tuple[bytes, ...], int] = defaultdict(int)
@@ -67,7 +67,8 @@ def _pre_tokenize(text: str, special_tokens: list[str]) -> dict[tuple[bytes, ...
     for sub_chunk in sub_chunks:
         # Run pre-tokenization on your chunk and store the counts for each pre-token
         for m in PRE_TOKENIZER_RE.finditer(sub_chunk):
-            token_count[tuple(BYTE_TABLE[b] for b in m.group().encode())] += 1
+            # token_count[tuple(BYTE_TABLE[b] for b in m.group().encode())] += 1
+            token_count[tuple(map(BYTE_TABLE.__getitem__, m.group().encode()))] += 1
 
     return token_count
 
